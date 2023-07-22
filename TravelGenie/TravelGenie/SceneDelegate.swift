cd3bab080005f4040aa38727b05d3c6a9cce1db1
woong <workplayhard1@naver.com>
2023-07-22 13:16:16 +0900

    feat: project 생성 및 스토리보드 제거, 타겟설정 15.0

---
+++ TravelGenie/TravelGenie/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  TravelGenie
+//
+//  Created by 서현웅 on 2023/07/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
---
+++ TravelGenie/TravelGenie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelGenie
+//
+//  Created by 서현웅 on 2023/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
