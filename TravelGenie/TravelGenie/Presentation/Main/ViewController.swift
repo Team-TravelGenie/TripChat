4ff5a9b0d0b25e891f245729777defe99bd6b07c
woong <workplayhard1@naver.com>
2023-07-22 19:19:48 +0900

    feat: 테스트용 메서드 임시구현, 테스트 이미지 추가

--- TravelGenie/TravelGenie/Presentation/Main/ViewController.swift
+++ TravelGenie/TravelGenie/Presentation/Main/ViewController.swift
@@ -11,9 +11,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        requestTest()
     }
-
-
 }
 
+// MARK: TestMethod 사용 후 제거
+extension ViewController {
+    func requestTest() {
+        let repo = DefaultRepository()
+        let imageToBase64StringEncoding = base64Encoding()
+        
+        repo.requestDetectedImageLabel(imageToBase64StringEncoding) { result in
+            switch result {
+            case .success(let response):
+                print(response)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func base64Encoding() -> String {
+        guard let image = UIImage(named: "samsungGalaxyLogo"),
+              let data = image.pngData() else { return "" }
+         return data.base64EncodedString()
+    }
+}
