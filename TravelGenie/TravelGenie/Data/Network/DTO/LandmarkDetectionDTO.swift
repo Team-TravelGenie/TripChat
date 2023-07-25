e67c5cf5429ea819d398b2cfe59758044d9636e8
woong <workplayhard1@naver.com>
2023-07-26 02:27:58 +0900

    feat: LandmarkDetectionDTO 구현

---
+++ TravelGenie/TravelGenie/Data/Network/DTO/LandmarkDetectionDTO.swift
@@ -0,0 +1,53 @@
+//
+//  LandmarkDetectionDTO.swift
+//  TravelGenie
+//
+//  Created by 서현웅 on 2023/07/26.
+//
+
+import Foundation
+
+struct LandmarkDetectionDTO: Decodable {
+    let responses: [Response]
+}
+
+extension LandmarkDetectionDTO {
+    struct Response: Decodable {
+        let landmarkAnnotations: [LandmarkAnnotation]
+    }
+}
+
+extension LandmarkDetectionDTO.Response {
+    struct LandmarkAnnotation: Decodable {
+        let mid: String
+        let description: String
+        let score: Double
+        let boundingPoly: BoundingPoly
+        let locations: [Location]
+    }
+}
+
+extension LandmarkDetectionDTO.Response.LandmarkAnnotation {
+    struct BoundingPoly: Decodable {
+        let vertices: [Vertex]
+    }
+    
+    struct Location: Decodable {
+        let latLng: LatLng
+    }
+}
+
+extension LandmarkDetectionDTO.Response.LandmarkAnnotation.BoundingPoly {
+    struct Vertex: Decodable {
+        let x: Int
+        let y: Int
+    }
+}
+
+extension LandmarkDetectionDTO.Response.LandmarkAnnotation.Location {
+    struct LatLng: Decodable {
+        let latitude: Double
+        let longitude: Double
+    }
+}
+
