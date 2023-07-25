1697759f7c89df9366bef163529fd979c94be090
woong <workplayhard1@naver.com>
2023-07-26 04:58:29 +0900

    feat: ViewController 테스트용 메서드 수정

--- TravelGenie/TravelGenie/Presentation/Main/ViewController.swift
+++ TravelGenie/TravelGenie/Presentation/Main/ViewController.swift
@@ -11,17 +11,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        requestTest()
+        requestDetectionLandmarkTest()
     }
 }
 
 // MARK: TestMethod 사용 후 제거
 extension ViewController {
-    func requestTest() {
+    func requestDetectionImageLabelTest() {
         let repo = GoogleVisionRepository()
         let imageToBase64StringEncoding = base64Encoding()
         
         repo.requestImageLabelDetection(imageToBase64StringEncoding) { result in
+            switch result {
+            case .success(let response):
+                print(response)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func requestDetectionLandmarkTest() {
+        let repo = GoogleVisionRepository()
+        let imageToBase64StringEncoding = base64Encoding()
+        
+        repo.requestLandmarkDetection(imageToBase64StringEncoding) { result in
             switch result {
             case .success(let response):
                 print(response)
