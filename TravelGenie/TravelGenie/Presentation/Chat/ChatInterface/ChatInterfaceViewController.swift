6f27592e19ed01bd479b37f6f1466a1f41569c38
woong <workplayhard1@naver.com>
2023-08-15 19:06:08 +0900

    feat: MessagesDataSource 채택

--- TravelGenie/TravelGenie/Presentation/Chat/ChatInterface/ChatInterfaceViewController.swift
+++ TravelGenie/TravelGenie/Presentation/Chat/ChatInterface/ChatInterfaceViewController.swift
@@ -14,4 +14,22 @@ class ChatInterfaceViewController: MessagesViewController {
         super.viewDidLoad()
 
     }
+    
+    private func setupMessagesCollectionViewAttributes() {
+        messagesCollectionView.messagesDataSource = self
+    }
+}
+
+extension ChatInterfaceViewController: MessagesDataSource {
+    var currentSender: MessageKit.SenderType {
+        <#code#>
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessageKit.MessagesCollectionView) -> MessageKit.MessageType {
+        <#code#>
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessageKit.MessagesCollectionView) -> Int {
+        <#code#>
+    }
 }
