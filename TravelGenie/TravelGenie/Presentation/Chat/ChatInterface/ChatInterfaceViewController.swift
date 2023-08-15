06ffd69b96c5437e14b7c5f49327e67748d1b26d
woong <workplayhard1@naver.com>
2023-08-15 19:01:41 +0900

    feat: ChatInterfaceViewControleller 생성 (Layout, Display, DataSource 담당목적)

---
+++ TravelGenie/TravelGenie/Presentation/Chat/ChatInterface/ChatInterfaceViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ChatInterfaceViewController.swift
+//  TravelGenie
+//
+//  Created by 서현웅 on 2023/08/15.
+//
+
+import UIKit
+import MessageKit
+
+class ChatInterfaceViewController: MessagesViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
