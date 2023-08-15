a147f6b63b3a45f5b0fc5f58fe6a5c76cd4fdb7f
summercat <summerly812@gmail.com>
2023-08-15 22:05:29 +0900

    feat: HomeVC, HomeVM 생성

---
+++ TravelGenie/TravelGenie/Presentation/Home/HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewController.swift
+//  TravelGenie
+//
+//  Created by summercat on 2023/08/14.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    private let viewModel: HomeViewModel
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    init(viewModel: HomeViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ TravelGenie/TravelGenie/Presentation/Home/HomeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewModel.swift
+//  TravelGenie
+//
+//  Created by summercat on 2023/08/14.
+//
+
+import Foundation
+
+final class HomeViewModel {
+    
+    weak var coordinator: HomeCoordinator?
+}
