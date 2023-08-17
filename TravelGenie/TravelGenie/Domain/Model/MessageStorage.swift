c9a97127ca493975930b3adc9a7276e8c5afd35a
woong <workplayhard1@naver.com>
2023-08-17 22:44:18 +0900

    styel: 순서컨벤션 준수

--- TravelGenie/TravelGenie/Domain/Model/MessageStorage.swift
+++ TravelGenie/TravelGenie/Domain/Model/MessageStorage.swift
@@ -12,21 +12,36 @@ final class MessageStorage {
         static let welcomeText = "오늘은 어디로 여행을 떠나고 싶나요? 사진을 보내주시면 원하는 분위기의 여행지를 추천해드릴게요!"
     }
     
+    // MARK: Property
+    
+    var count: Int {
+        return messageList.count
+    }
+    var didChangedMessageList: (() -> Void)?
+
     private var messageList: [Message] = [Message]() {
         didSet {
             didChangedMessageList?()
         }
     }
     
-    var count: Int {
-        return messageList.count
-    }
-    
-    var didChangedMessageList: (() -> Void)?
-    
+    // MARK: Initializer
+
     init() {
         self.messageList = setupDefaultMessage()
     }
+    
+    // MARK: Internal
+    
+    func insertMessage(_ message: Message) {
+        messageList.append(message)
+    }
+    
+    func sectionOfMessageList(_ section: Int) -> Message {
+        return messageList[section]
+    }
+    
+    // MARK: Private
     
     private func setupDefaultMessage() -> [Message] {
         let defaultMessages = [
@@ -36,15 +51,5 @@ final class MessageStorage {
         ]
         
         return defaultMessages
-    }
-    
-    // MARK: 외부함수 - (메시지값 변경 / 읽기)
-    
-    func insertMessage(_ message: Message) {
-        messageList.append(message)
-    }
-    
-    func sectionOfMessageList(_ section: Int) -> Message {
-        return messageList[section]
     }
 }
