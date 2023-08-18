9dff43be7885a4a9c41692c4673d753acb928fb9
summercat <summerly812@gmail.com>
2023-08-18 14:30:13 +0900

    style: AppDelegate, SceneDelegate 컨벤션 수정

--- TravelGenie/TravelGenie/Application/SceneDelegate.swift
+++ TravelGenie/TravelGenie/Application/SceneDelegate.swift
@@ -12,7 +12,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     var coordinator: AppCoordinator?
     
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions)
+    {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
