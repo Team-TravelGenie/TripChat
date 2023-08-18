55707a0dc1f50652ec2c1a472b9bce4600cddbfb
woong <workplayhard1@naver.com>
2023-08-18 15:52:20 +0900

    feat: CustomTextLayoutSizeCalculator 초기구현(수정필요함)

---
+++ TravelGenie/TravelGenie/Presentation/Chat/Cell/CustomTagCell/SizeCalculator/CustomTextLayoutSizeCalculator.swift
@@ -0,0 +1,78 @@
+//
+//  CustomTextLayoutSizeCalculator.swift
+//  TravelGenie
+//
+//  Created by 서현웅 on 2023/08/18.
+//
+
+import MessageKit
+import UIKit
+
+final class CustomTextLayoutSizeCalculator: CustomLayoutSizeCalculator {
+    var messageLabelFont = UIFont.preferredFont(forTextStyle: .body)
+    var cellMessageContainerRightSpacing: CGFloat = 16
+    
+    override func messageContainerSize(
+        for message: MessageType,
+        at indexPath: IndexPath)
+    -> CGSize
+    {
+        let size = super.messageContainerSize(
+            for: message,
+            at: indexPath)
+        let labelSize = messageLabelSize(
+            for: message,
+            at: indexPath)
+        let selfWidth = labelSize.width +
+        cellMessageContentHorizontalPadding +
+        cellMessageContainerRightSpacing
+        let width = max(selfWidth, size.width)
+        let height = size.height + labelSize.height
+        
+        return CGSize(
+            width: width,
+            height: height)
+    }
+    
+    func messageLabelSize(
+        for message: MessageType,
+        at _: IndexPath)
+    -> CGSize
+    {
+        let attributedText: NSAttributedString
+        
+        let textMessageKind = message.kind
+        switch textMessageKind {
+        case .attributedText(let text):
+            attributedText = text
+        case .text(let text), .emoji(let text):
+            attributedText = NSAttributedString(string: text, attributes: [.font: messageLabelFont])
+        default:
+            fatalError("messageLabelSize received unhandled MessageDataType: \(message.kind)")
+        }
+        
+        let maxWidth = messageContainerMaxWidth -
+        cellMessageContentHorizontalPadding -
+        cellMessageContainerRightSpacing
+        
+        return attributedText.size(consideringWidth: maxWidth)
+    }
+    
+    func messageLabelFrame(
+        for message: MessageType,
+        at indexPath: IndexPath)
+    -> CGRect
+    {
+        let origin = CGPoint(
+            x: cellMessageContentHorizontalPadding / 2,
+            y: cellMessageContentVerticalPadding / 2)
+        let size = messageLabelSize(
+            for: message,
+            at: indexPath)
+        
+        return CGRect(
+            origin: origin,
+            size: size)
+    }
+}
+
