f7e49f4ed3e8e1a73e21b9a75baa5ae553267f19
summercat <summerly812@gmail.com>
2023-08-20 20:02:57 +0900

    feat: RecommendationCellViewModel 생성

---
+++ TravelGenie/TravelGenie/Presentation/Chat/RecommendationCellViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  RecommendationCellViewModel.swift
+//  TravelGenie
+//
+//  Created by summercat on 2023/08/20.
+//
+
+final class RecommendationCellViewModel {
+    
+    var recommendations: [RecommendationItem] = []
+}
