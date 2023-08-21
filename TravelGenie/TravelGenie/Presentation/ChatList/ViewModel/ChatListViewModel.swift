1d8ca695b88c5298f31e1eb3b1417d28893aeffc
summercat <summerly812@gmail.com>
2023-08-21 17:31:37 +0900

    chore: ChatList 화면 더미 데이터 추가

--- TravelGenie/TravelGenie/Presentation/ChatList/ViewModel/ChatListViewModel.swift
+++ TravelGenie/TravelGenie/Presentation/ChatList/ViewModel/ChatListViewModel.swift
@@ -17,9 +17,22 @@ final class ChatListViewModel {
     
     init(chats: [Chat]) {
         self.chats = chats
+        addChat()
+    }
+    
     // MARK: Internal
     func deleteItem(at row: Int) {
         
     }
+    
+    // MARK: Private
+    
+    private func addChat() {
+        let chat1 = Chat(date: Date(), tags: ["바다", "동남아", "휴양지"])
+        let chat2 = Chat(date: Date(), tags: ["바다다", "동남아", "휴양지"])
+        let chat3 = Chat(date: Date(), tags: ["바다다다", "동남아", "휴양지"])
+        self.chats.append(chat1)
+        self.chats.append(chat2)
+        self.chats.append(chat3)
     }
 }
