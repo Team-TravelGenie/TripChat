ac7960295c1b9bfd9e0de0fa8994ae4a62e7a6d0
summercat <summerly812@gmail.com>
2023-08-23 21:59:57 +0900

    refactor: ImageMediaItem 타입 프로퍼티 추가, 이니셜라이저 수정
    - 코어데이터에서 복원 시 활용할 Data 프로퍼티 추가
    - UIImage 또는 Data 중 하나를 이용해 이니셜라이즈 가능하도록 이니셜라이저 수정

--- TravelGenie/TravelGenie/Domain/Entity/ContentItem.swift
+++ TravelGenie/TravelGenie/Domain/Entity/ContentItem.swift
@@ -16,11 +16,23 @@ struct ImageMediaItem: MediaItem {
     var image: UIImage?
     var placeholderImage: UIImage
     var size: CGSize
+    var imageData: Data?
     
-    init(image: UIImage) {
-        self.image = image
-        size = image.size // 디자인가이드를 현님에게 받아서 사이즈를 결정해야함
+    init(url: URL?, image: UIImage? = nil, imageData: Data? = nil) {
+        self.url = url
         placeholderImage = UIImage()
+        
+        if image == nil, imageData != nil {
+            let imageFromData = UIImage(data: imageData!)
+            self.image = imageFromData
+            size = imageFromData?.size ?? .init()
+        } else if image != nil, imageData == nil {
+            let dataFromImage = image?.pngData()
+            self.imageData = dataFromImage
+            size = image!.size
+        } else {
+            size = .init()
+        }
     }
 }
 
