da7a2098144c78262b51f52914db150b7b19bf7a
summercat <summerly812@gmail.com>
2023-08-23 18:13:52 +0900

    feat: CoreDataStorage 타입 내 코어 데이터 스택 구현

--- TravelGenie/TravelGenie/Data/PersistentStorage/CoreDataStorage/CoreDataStorage.swift
+++ TravelGenie/TravelGenie/Data/PersistentStorage/CoreDataStorage/CoreDataStorage.swift
@@ -5,9 +5,37 @@
 //  Created by summercat on 2023/08/22.
 //
 
+import CoreData
 import Foundation
 
 final class CoreDataStorage {
     
     static let shared = CoreDataStorage()
+    
+    private lazy var persistentContainer: NSPersistentContainer = {
+        MessageDAOTransformer.register()
+        
+        let container = NSPersistentContainer(name: "CoreDataModel")
+        container.loadPersistentStores { description, error in
+            if let error = error as NSError? {
+                assertionFailure("CoreDataStorage \(error)")
+            }
+        }
+        
+        return container
+    }()
+    
+    private lazy var context: NSManagedObjectContext = {
+        return self.persistentContainer.newBackgroundContext()
+    }()
+    
+    private func saveContext() {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                print(StorageError.coreDataSaveFailure(error))
+            }
+        }
+    }
 }
