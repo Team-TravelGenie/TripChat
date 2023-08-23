5121de1d21325f622838c3cc363c9c2b72334014
summercat <summerly812@gmail.com>
2023-08-23 18:29:26 +0900

    feat: CoreData create 메서드 구현
    - Chat 타입을 코어데이터에 ChatEntity 타입으로 저장하는 메서드

--- TravelGenie/TravelGenie/Data/PersistentStorage/CoreDataStorage/CoreDataStorage.swift
+++ TravelGenie/TravelGenie/Data/PersistentStorage/CoreDataStorage/CoreDataStorage.swift
@@ -28,6 +28,64 @@ final class CoreDataStorage {
     private lazy var context: NSManagedObjectContext = {
         return self.persistentContainer.newBackgroundContext()
     }()
+
+    func create(_ chat: Chat) {
+        let tags: [String] = chat.tags
+        let messages: [Message] = chat.messages
+        let recommendations: [String] = chat.recommendations
+        let chatEntity = ChatEntity(context: context)
+        
+        chatEntity.setValue(chat.id, forKey: "id")
+        chatEntity.setValue(chat.createdAt, forKey: "createdAt")
+        
+        tags.forEach {
+            let tagEntity = TagEntity(context: context)
+            tagEntity.value = $0
+            chatEntity.addToTags(tagEntity)
+        }
+        
+        recommendations.forEach {
+            let recommendationEntity = RecommendationEntity(context: context)
+            recommendationEntity.value = $0
+            chatEntity.addToRecommendations(recommendationEntity)
+        }
+        
+        messages.forEach {
+            var data: Data? = nil
+            let messageEntity = MessageEntity(context: context)
+            messageEntity.setValue(UUID(uuidString: $0.messageId)!, forKey: "id")
+            messageEntity.setValue($0.kind.description, forKey: "kind")
+            messageEntity.setValue($0.sender.displayName, forKey: "sender")
+            messageEntity.setValue($0.sentDate, forKey: "sentDate")
+            
+            switch $0.kind {
+            case .text(let text):
+                data = try? JSONEncoder().encode(text)
+            case .attributedText(let attributedText):
+                data = try? NSKeyedArchiver.archivedData(withRootObject: attributedText, requiringSecureCoding: true)
+            case .photo(let mediaItem):
+                let dao = MediaItemDAO(with: mediaItem)
+                data = try? JSONEncoder().encode(dao)
+            case .video(let mediaItem):
+                let dao = MediaItemDAO(with: mediaItem)
+                data = try? JSONEncoder().encode(dao)
+            case .custom(let item):
+                if $0.kind.description == "tag" {
+                    let item = item as? TagItem
+                    data = try? JSONEncoder().encode(item)
+                } else if $0.kind.description == "recommendation" {
+                    let item = item as? RecommendationItem
+                    data = try? JSONEncoder().encode(item)
+                }
+            default:
+                break
+            }
+            
+            messageEntity.setValue(data, forKey: "data")
+        }
+
+        saveContext()
+    }
     
     private func saveContext() {
         if context.hasChanges {
