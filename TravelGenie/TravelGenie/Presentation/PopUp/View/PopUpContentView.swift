427857586eb8c4463f7cdbc55322aa3159abc387
summercat <summerly812@gmail.com>
2023-08-27 18:48:47 +0900

    feat: PopUpContentView 구현

--- TravelGenie/TravelGenie/Presentation/PopUp/View/PopUpContentView.swift
+++ TravelGenie/TravelGenie/Presentation/PopUp/View/PopUpContentView.swift
@@ -21,6 +21,38 @@ final class PopUpContentView: UIView {
     
     let type: PopUpType?
     
+    private let mainStackView = UIStackView()
+    private let closeButtonStackView = UIStackView()
+    private let iconAndMainTextStackView = UIStackView()
+    private let feedbackButtonStackView = UIStackView()
+    private let bottomButtonStackView = UIStackView()
+    private let closeButton = UIButton()
+    private let mainTextView = UITextView()
+    private let feedbackTextView = FeedbackTextView()
+    
+    private let chatIconView = CircleIconView()
+        .size(40)
+        .backgroundColor(.blueGrayBackground)
+        .iconImage(imageName: "chat", size: 32)
+    
+    private let thumbsUpButton = FeedbackButton()
+        .size(72)
+        .image(name: "thumbs-up-regular", size: 32)
+    
+    private let thumbsDownButton = FeedbackButton()
+        .size(72)
+        .image(name: "thumbs-down-regular", size: 32)
+    
+    private let leftButton = RectangleTextButton()
+        .cornerRadius(8)
+        .backgroundColor(.primary)
+    
+    private let rightButton = RectangleTextButton()
+        .backgroundColor(.white)
+        .borderColor(.blueGrayLine)
+        .borderWidth(1)
+        .cornerRadius(8)
+    
     // MARK: Lifecycle
     
     init(type: PopUpType) {
@@ -29,9 +61,138 @@ final class PopUpContentView: UIView {
         backgroundColor = .white
         layer.cornerRadius = 20
         layer.masksToBounds = true
+        configureSubviews()
+        configureHierarchy()
+        configureLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Private
+    
+    private func configureSubviews() {
+        configureMainStackView()
+        configureCloseButtonStackView()
+        configureMessageStackView()
+        configureFeedbackButtonStackView()
+        configureBottomButtonStackView()
+    }
+    
+    private func configureMainStackView() {
+        mainStackView.axis = .vertical
+        mainStackView.spacing = 24
+    }
+    
+    private func configureCloseButtonStackView() {
+        closeButtonStackView.axis = .vertical
+        closeButtonStackView.alignment = .trailing
+        configureCloseButton()
+    }
+    
+    private func configureCloseButton() {
+        let buttonImage = UIImage(systemName: "xmark")
+        closeButton.setImage(buttonImage, for: .normal)
+        closeButton.tintColor = .black
+    }
+    
+    private func configureMessageStackView() {
+        iconAndMainTextStackView.spacing = 8
+        iconAndMainTextStackView.alignment = .top
+        iconAndMainTextStackView.axis = .horizontal
+        configureDescriptionTextView()
+    }
+    
+    private func configureDescriptionTextView() {
+        mainTextView.isEditable = false
+        mainTextView.isScrollEnabled = false
+        mainTextView.backgroundColor = .clear
+        mainTextView.textContainerInset = UIEdgeInsets(top: .zero, left: .zero, bottom: .zero, right: .zero)
+    }
+    
+    private func configureFeedbackButtonStackView() {
+        feedbackButtonStackView.spacing = 12
+        feedbackButtonStackView.axis = .horizontal
+        feedbackButtonStackView.distribution = .equalCentering
+    }
+        
+    private func configureBottomButtonStackView() {
+        bottomButtonStackView.spacing = 8
+        bottomButtonStackView.axis = .horizontal
+        bottomButtonStackView.distribution = .fillEqually
+    }
+    
+    private func configureHierarchy() {
+        addSubview(mainStackView)
+        closeButtonStackView.addArrangedSubview(closeButton)
+
+        [
+            closeButtonStackView,
+            iconAndMainTextStackView,
+            bottomButtonStackView,
+        ].forEach { mainStackView.addArrangedSubview($0) }
+        
+        
+        [
+            chatIconView,
+            mainTextView,
+        ].forEach { iconAndMainTextStackView.addArrangedSubview($0) }
+        
+        [
+            UIView(),
+            thumbsUpButton,
+            thumbsDownButton,
+            UIView(),
+        ].forEach { feedbackButtonStackView.addArrangedSubview($0) }
+        
+        
+        [
+            leftButton,
+            rightButton,
+        ].forEach { bottomButtonStackView.addArrangedSubview($0) }
+        
+        if case .feedback = type {
+            [feedbackTextView, feedbackButtonStackView].forEach {
+                mainStackView.insertArrangedSubview($0, at: 2)
+            }
+        }
+    }
+    
+    private func configureLayout() {
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            mainStackView.topAnchor.constraint(equalTo: topAnchor, constant: 24),
+            mainStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -24),
+            mainStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+            mainStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+        ])
+        
+        thumbsUpButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            thumbsUpButton.widthAnchor.constraint(equalToConstant: 72),
+            thumbsUpButton.heightAnchor.constraint(equalToConstant: 72),
+        ])
+        
+        thumbsDownButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            thumbsDownButton.widthAnchor.constraint(equalToConstant: 72),
+            thumbsDownButton.heightAnchor.constraint(equalToConstant: 72),
+        ])
+        
+        feedbackTextView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            feedbackTextView.heightAnchor.constraint(equalToConstant: 100),
+        ])
+        
+        leftButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            leftButton.heightAnchor.constraint(equalToConstant: 47),
+        ])
+        
+        rightButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            rightButton.heightAnchor.constraint(equalToConstant: 47),
+        ])
     }
 }
