5df74d5e48681fb979497fa80721e0c1b70f2e0b
summercat <summerly812@gmail.com>
2023-08-28 22:07:23 +0900

    feat: ChatInterfaceVM 생성

---
+++ TravelGenie/TravelGenie/Presentation/Chat/ViewModel/ChatInterfaceViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  ChatInterfaceViewModel.swift
+//  TravelGenie
+//
+//  Created by summercat on 2023/08/28.
+//
+
+final class ChatInterfaceViewModel {
+    
+    let messageStorage: MessageStorage = MessageStorage()
+}
