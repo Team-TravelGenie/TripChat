766883e92327835c6ae58b3df7508d7a461a09e1
woong <workplayhard1@naver.com>
2023-08-29 20:42:48 +0900

    feat: viewModel의 버튼 상태 변수가 변화하면 버튼셀 섹션을 리로드하도록 구현

--- TravelGenie/TravelGenie/Presentation/Chat/View/ChatInterface/ChatInterfaceViewController.swift
+++ TravelGenie/TravelGenie/Presentation/Chat/View/ChatInterface/ChatInterfaceViewController.swift
@@ -52,6 +52,7 @@ class ChatInterfaceViewController: MessagesViewController {
                 return messagesCollectionView.dequeueReusableCell(SystemMessageCell.self, for: indexPath)
             case .uploadButtonMessage:
                 let cell = messagesCollectionView.dequeueReusableCell(UploadButtonCell.self, for: indexPath)
+                cell.configureButtonState(state: chatInterfaceViewModel.uploadButtonState)
                 return cell
             }
         }
@@ -91,6 +92,17 @@ class ChatInterfaceViewController: MessagesViewController {
         chatInterfaceViewModel.messageStorage.didChangedMessageList = { [weak self] in
             DispatchQueue.main.async {
                 self?.messagesCollectionView.reloadData()
+            }
+        }
+        
+        chatInterfaceViewModel.didchangeUploadButtonState = { [weak self] state in
+            let uploadButtonCellSectionIndex = MessagesDefaultSection.uploadButtonMessage.rawValue
+            let indexSet = IndexSet(integer: uploadButtonCellSectionIndex)
+
+            DispatchQueue.main.async {
+                UIView.performWithoutAnimation {
+                    self?.messagesCollectionView.reloadSections(indexSet)
+                }
             }
         }
     }
