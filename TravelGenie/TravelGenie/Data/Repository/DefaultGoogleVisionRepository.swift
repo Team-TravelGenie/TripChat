c45db86088d69b3e6ad39bfe956afdbcc9f679fa
woong <workplayhard1@naver.com>
2023-08-29 21:53:05 +0900

    feat: defaultGoogleRepository가 GoogleVisionRepository Interface를 채택

--- TravelGenie/TravelGenie/Data/Repository/DefaultGoogleVisionRepository.swift
+++ TravelGenie/TravelGenie/Data/Repository/DefaultGoogleVisionRepository.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Moya
 
-final class DefaultGoogleVisionRepository {
+final class DefaultGoogleVisionRepository: GoogleVisionRepository {
     private let networkService = NetworkService()
     
     func requestImageLabelDetection(
