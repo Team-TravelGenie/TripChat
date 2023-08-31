19ede57b99f65f6e546475e1dd0d764ec7ba4488
woong <workplayhard1@naver.com>
2023-09-01 03:26:59 +0900

    feat: DefaultTranslateRepository 초기구현

---
+++ TravelGenie/TravelGenie/Data/Repository/DefaultTranslateRepository.swift
@@ -0,0 +1,29 @@
+//
+//  DefaultTranslateRepository.swift
+//  TravelGenie
+//
+//  Created by 서현웅 on 2023/09/01.
+//
+
+import Foundation
+
+final class DefaultTranslateRepository {
+    
+    private let networkService = NetworkService()
+    
+    func translate(
+        with kewords: String,
+        completion: @escaping ((Result<String, Error>) -> Void))
+    {
+        let requestModel = PapagoTranslateRequestModel(text: kewords)
+        
+        networkService.request(PapagoTranslateAPI.translate(requestModel)) { result in
+            switch result {
+            case .success(let response):
+                completion(.success(response.message.result.translatedText))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
