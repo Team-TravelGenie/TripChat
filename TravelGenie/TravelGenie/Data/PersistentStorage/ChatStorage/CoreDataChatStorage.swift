0ce64f4c0339cb177491d4186abe9ae2aecbad88
summercat <summerly812@gmail.com>
2023-08-31 17:07:41 +0900

    feat: CoreDataChatStorage의 fetchChats 메서드 구현

--- TravelGenie/TravelGenie/Data/PersistentStorage/ChatStorage/CoreDataChatStorage.swift
+++ TravelGenie/TravelGenie/Data/PersistentStorage/ChatStorage/CoreDataChatStorage.swift
@@ -33,9 +33,65 @@ extension CoreDataChatStorage: ChatStorage {
         with keyword: String,
         completion: @escaping (Result<[Chat], Error>) -> Void)
     {
+        let tagResult: [Chat] = fetchWithTag(keyword)
+        let recommendationResult: [Chat] = fetchWithRecommendation(keyword)
+        let result: [Chat] = tagResult + recommendationResult
+        
+        if result.isEmpty {
+            completion(.failure(StorageError.noResultForKeyword))
+        } else {
+            completion(.success(result))
+        }
     }
     
     func deleteChat(with id: UUID, completion: @escaping (Result<Bool, Error>) -> Void) {
         
+    }
+    
+    // MARK: Private
+    
+    private func fetchWithTag(_ tag: String) -> [Chat] {
+        var result: [Chat] = []
+        let request = TagEntity.fetchRequest()
+        let predicate = NSPredicate(format: "value CONTAINS[c] %@", tag)
+        
+        do {
+            let tagEntities: [TagEntity] = try coreDataService.fetch(request: request, predicate: predicate)
+            tagEntities.forEach {
+                if let chatEntities = $0.chat?.array as? [ChatEntity] {
+                    result.append(contentsOf: chatEntities.map { $0.toDomain() })
+                }
+            }
+            
+            return result
+        } catch {
+            return result
+        }
+    }
+    
+    private func fetchWithRecommendation(_ keyword: String) -> [Chat] {
+        var result: [Chat] = []
+        let request = RecommendationEntity.fetchRequest()
+        let countryPredicate = NSPredicate(format: "country CONTAINS[c] %@", keyword)
+        let spotPredicate = NSPredicate(format: "spot CONTAINS[c] %@", keyword)
+        let predicate = NSCompoundPredicate(
+            type: .or,
+            subpredicates: [
+                countryPredicate,
+                spotPredicate,
+            ])
+        
+        do {
+            let recommendationEntities: [RecommendationEntity] = try coreDataService.fetch(request: request, predicate: predicate)
+            recommendationEntities.forEach {
+                if let chatEntities = $0.chat?.array as? [ChatEntity] {
+                    result.append(contentsOf: chatEntities.map { $0.toDomain()} )
+                }
+            }
+            
+            return result
+        } catch {
+            return result
+        }
     }
 }
