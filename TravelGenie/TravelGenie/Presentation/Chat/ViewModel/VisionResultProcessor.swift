a1c306fcf56aea55cec6f2755bc079bb71e30671
woong <workplayhard1@naver.com>
2023-09-01 17:03:13 +0900

    feat: 6개 컨피던스 순 정렬 -> 번역 -> 중복값 제거 하여 [Tag]타입으로 반환하도록 로직 구현

--- TravelGenie/TravelGenie/Presentation/Chat/ViewModel/VisionResultProcessor.swift
+++ TravelGenie/TravelGenie/Presentation/Chat/ViewModel/VisionResultProcessor.swift
@@ -24,32 +24,51 @@ final class VisionResultProcessor {
         visionResults.landmarks.append(contentsOf: landmarks)
     }
     
-    func getTopSixResults() -> [Tag] {
-        var topSixResults: [Any] = []
+    func getSixMostConfidentTranslatedTags(useCase: TranslateUseCase, completion: @escaping ([Tag]) -> Void) {
+        let sortedVisionResult = sortByConfidence()
         
-        topSixResults.append(contentsOf: visionResults.landmarks)
-        
-        let sortedKeywords = visionResults.keywords.sorted(by: { $0.confidence > $1.confidence })
-        let remainingSpace = 6 - topSixResults.count
-        
-        if remainingSpace > 0 {
-            let remainingKeywords = Array(sortedKeywords.prefix(remainingSpace))
+        translate(keyword: sortedVisionResult, useCase: useCase) { [weak self] result in
+            guard let self else { return }
             
-            topSixResults.append(contentsOf: remainingKeywords)
-        }
-        
-        return convertTags(topSixResults)
-    }
-    
-    private func convertTags(_ results: [Any]) -> [Tag] {
-        return results.compactMap {
-            if let landmark = $0 as? Landmark {
-                return Tag(category: .theme, value: landmark.place)
-            } else if let keyword = $0 as? Keyword {
-                return Tag(category: .theme, value: keyword.name)
+            var deDuplicatedResult = self.removeDuplicateValues(text: result)
+            
+            if deDuplicatedResult.count > 6 {
+                deDuplicatedResult = Array(deDuplicatedResult.prefix(6))
             }
             
-            return nil
+            let topSixTags = self.convertSeperatedTextToTags(texts: deDuplicatedResult)
+            
+            completion(topSixTags)
         }
+    }
+    
+    private func sortByConfidence() -> String {
+        let sortedLandmarks = visionResults.landmarks.sorted(by: { $0.confidence > $1.confidence }).map { $0.place }
+        let sortedKeywords = visionResults.keywords.sorted(by: { $0.confidence > $1.confidence }).map { $0.name }
+        let combinedResults = sortedLandmarks + sortedKeywords
+        
+        return combinedResults.joined(separator: ",")
+    }
+    
+    private func removeDuplicateValues(text: String) -> [String] {
+        let results = text.split(separator: ",")
+        let trimmingResults = results.map { String($0).replacingOccurrences(of: " ", with: "") }
+        
+        return Array(Set(trimmingResults))
+    }
+    
+    private func translate(keyword: String, useCase: TranslateUseCase, completion: @escaping (String) -> Void) {
+        useCase.translateKeywords(with: keyword) { result in
+            switch result {
+            case .success(let translatedKeyword):
+                completion(translatedKeyword)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    private func convertSeperatedTextToTags(texts: [String]) -> [Tag] {
+        return texts.map { Tag(category: .theme, value: $0) }
     }
 }
