2b607b6dfff0ab9263f42bee0aeb0963532bb9f6
woong <workplayhard1@naver.com>
2023-09-06 15:39:39 +0900

    feat: OpenAIPrompt를 Constant 내부로 이동

--- TravelGenie/TravelGenie/Presentation/Chat/ViewModel/ChatViewModel.swift
+++ TravelGenie/TravelGenie/Presentation/Chat/ViewModel/ChatViewModel.swift
@@ -23,9 +23,7 @@ final class ChatViewModel {
     
     private enum Constant {
         static let welcomeText = "오늘은 어디로 여행을 떠나고 싶나요? 사진을 보내주시면 원하는 분위기의 여행지를 추천해드릴게요!"
-    }
-    
-    private enum OpenAIPrompt {
+        
         static let openAISystemPrompt: String = """
             당신은 사용자가 입력한 키워드에 기반해서 3개의 여행지를 추천해주는 챗봇입니다.
             차근차근 생각해 봅시다.
@@ -313,7 +311,7 @@ final class ChatViewModel {
     // MARK: OpenAI
     
     private func addDefaultOpenAIPropmpt() {
-        let message = ChatMessage(role: .system, content: OpenAIPrompt.openAISystemPrompt)
+        let message = ChatMessage(role: .system, content: Constant.openAISystemPrompt)
         openAIChatMessages.append(message)
     }
     
