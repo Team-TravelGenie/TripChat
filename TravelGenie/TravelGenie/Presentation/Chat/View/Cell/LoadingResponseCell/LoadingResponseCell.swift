a01be96d5a3b9011d7f377dbfb9d4d866a1426bc
woong <workplayhard1@naver.com>
2023-09-07 19:41:31 +0900

    feat: LoadingResponseCell 생성

---
+++ TravelGenie/TravelGenie/Presentation/Chat/View/Cell/LoadingResponseCell/LoadingResponseCell.swift
@@ -0,0 +1,14 @@
+//
+//  LoadingResponseCell.swift
+//  TravelGenie
+//
+//  Created by 서현웅 on 2023/09/07.
+//
+
+import Foundation
+import UIKit
+
+final class LoadingResponseCell: UICollectionViewCell {
+    
+    
+}
