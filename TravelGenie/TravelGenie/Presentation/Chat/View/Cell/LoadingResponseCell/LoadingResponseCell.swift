1c0dbb862c789d342e69d4270e58c653c8e35f87
woong <workplayhard1@naver.com>
2023-09-07 19:51:14 +0900

    feat: animationView 정의 및 configure

--- TravelGenie/TravelGenie/Presentation/Chat/View/Cell/LoadingResponseCell/LoadingResponseCell.swift
+++ TravelGenie/TravelGenie/Presentation/Chat/View/Cell/LoadingResponseCell/LoadingResponseCell.swift
@@ -5,10 +5,24 @@
 //  Created by 서현웅 on 2023/09/07.
 //
 
-import Foundation
 import UIKit
+import Lottie
 
 final class LoadingResponseCell: UICollectionViewCell {
     
+    private let animationView = LottieAnimationView(name: "iHi4RYS3KJ-2")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureAnimationView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureAnimationView() {
+        animationView.play()
+    }
     
 }
