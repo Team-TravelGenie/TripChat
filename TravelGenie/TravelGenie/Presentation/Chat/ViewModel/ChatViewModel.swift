b8e41e255203ec2bf33ddbc0878ae2ad5eed73ad
woong <workplayhard1@naver.com>
2023-09-11 13:43:36 +0900

    feat: extractKeyword(landmark/imagelabel API), insertRecommendation(imageSearchAPI), sendMessageToOpenAI(openAI API) 구간에서 로딩메시지 삽입 및 제거 구현

--- TravelGenie/TravelGenie/Presentation/Chat/ViewModel/ChatViewModel.swift
+++ TravelGenie/TravelGenie/Presentation/Chat/ViewModel/ChatViewModel.swift
@@ -174,6 +174,7 @@ final class ChatViewModel {
     }
     
     private func extractKeywords(from imageData: [Data]) {
+        insertLoadingMessage()
         let group = DispatchGroup()
         
         imageData.forEach {
@@ -199,6 +200,7 @@ final class ChatViewModel {
                 let appendedTags = defaultTags + $0
                 let tagMessage = self.createTagMessage(from: appendedTags)
                 
+                deleteLoadingMessage()
                 insertMessage(tagMessage)
             }
         }
@@ -263,6 +265,8 @@ final class ChatViewModel {
     }
     
     private func insertRecommendationMessage(with result: OpenAIRecommendation) {
+        insertLoadingMessage()
+        
         let items = result.recommendationItems
         let group = DispatchGroup()
         
@@ -277,6 +281,7 @@ final class ChatViewModel {
         group.notify(queue: .main) { [weak self] in
             guard let self else { return }
             let message = Message(recommendations: self.recommendationItems)
+            deleteLoadingMessage()
             insertMessage(message)
         }
     }
@@ -303,6 +308,20 @@ final class ChatViewModel {
         }
     }
     
+    private func insertLoadingMessage() {
+        let loadingMessage = createLoadingMessage()
+        
+        insertMessage(loadingMessage)
+    }
+    
+    private func deleteLoadingMessage() {
+        messageStorageDelegate?.deleteLoadingMessage()
+    }
+    
+    private func createLoadingMessage() -> Message {
+        return Message(sender: ai, messageId: "loading", sentDate: Date())
+    }
+    
     // MARK: OpenAI
     
     private func addDefaultOpenAIPropmpt() {
@@ -316,11 +335,12 @@ final class ChatViewModel {
     }
     
     private func sendMessageToOpenAI(_ message: ChatMessage) {
-        // TODO: - 챗지피티에 메시지 발송할 때 항상 답변 생성 애니메이션 넣어야 함
+        insertLoadingMessage()
         openAIChatMessages.append(message)
         openAIUseCase.send(chatMessages: openAIChatMessages) { [weak self] result in
             switch result {
             case .success(let chatMessages):
+                self?.deleteLoadingMessage()
                 self?.openAIChatMessages.append(contentsOf: chatMessages)
                 self?.configureOpenAIResponse(chatMessages)
             case .failure(let error):
