8b82438bec7f0b0c8bd55d2ffc41d7827108f04c
summercat <summerly812@gmail.com>
2023-09-12 16:33:16 +0900

    refactor: compress 메서드 내부 구현 수정
    - `UIGraphicsBeginImageContextWithOptions`가 iOS17.0에서 deprecated 될 예정이기 때문에, `UIGraphicsImageRenderer`를 활용해 기존에 구현해 놓은 `resize` 메서드를 활용하도록 리팩토링

--- TravelGenie/TravelGenie/Utility/ImageCompressor.swift
+++ TravelGenie/TravelGenie/Utility/ImageCompressor.swift
@@ -31,10 +31,7 @@ struct ImageCompressor {
                     width: image.size.width * iterationCompression,
                     height: image.size.height * iterationCompression)
                 
-                UIGraphicsBeginImageContextWithOptions(canvasSize, false, image.scale)
-                defer { UIGraphicsEndImageContext() }
-                image.draw(in: CGRect(origin: .zero, size: canvasSize))
-                iterationImage = UIGraphicsGetImageFromCurrentImageContext()
+                iterationImage = image.resize(width: canvasSize.width, height: canvasSize.height)
                 
                 guard let newImageSize = iterationImage?.jpegData(compressionQuality: 1.0)?.count else {
                     return completion(nil)
