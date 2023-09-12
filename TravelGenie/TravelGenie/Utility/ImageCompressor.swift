2b94ee4c3399a31df8a7075dfa3d9865785efd59
summercat <summerly812@gmail.com>
2023-09-12 13:46:37 +0900

    feat: ImageCompressor 구조체 구현

---
+++ TravelGenie/TravelGenie/Utility/ImageCompressor.swift
@@ -0,0 +1,59 @@
+//
+//  ImageCompressor.swift
+//  TravelGenie
+//
+//  Created by summercat on 2023/09/11.
+//
+
+import UIKit
+
+struct ImageCompressor {
+    static func compress(
+        image: UIImage,
+        maxByte: Int,
+        completion: @escaping (Data?) -> Void)
+    {
+        DispatchQueue.global().async {
+            guard let currentImageSize = image.jpegData(compressionQuality: 1.0)?.count else {
+                return completion(nil)
+            }
+            
+            var iterationImage: UIImage? = image
+            var iterationImageSize = currentImageSize
+            var iterationCompression: CGFloat = 1.0
+            
+            while iterationImageSize > maxByte,
+                  iterationCompression > 0.01 {
+                let percentageDecrease = getPercentageToDecreaseTo(forDataCount: iterationImageSize)
+                let canvasSize = CGSize(
+                    width: image.size.width * iterationCompression,
+                    height: image.size.height * iterationCompression)
+                
+                UIGraphicsBeginImageContextWithOptions(canvasSize, false, image.scale)
+                defer { UIGraphicsEndImageContext() }
+                image.draw(in: CGRect(origin: .zero, size: canvasSize))
+                iterationImage = UIGraphicsGetImageFromCurrentImageContext()
+                
+                guard let newImageSize = iterationImage?.jpegData(compressionQuality: 1.0)?.count else {
+                    return completion(nil)
+                }
+                
+                iterationImageSize = newImageSize
+                iterationCompression -= percentageDecrease
+            }
+            
+            completion(iterationImage?.jpegData(compressionQuality: 1.0))
+        }
+    }
+    
+    private static func getPercentageToDecreaseTo(forDataCount dataCount: Int) -> CGFloat {
+        switch dataCount {
+        case 0..<5_000_000:
+            return 0.03
+        case 5_000_000..<10_000_000:
+            return 0.1
+        default:
+            return 0.2
+        }
+    }
+}
