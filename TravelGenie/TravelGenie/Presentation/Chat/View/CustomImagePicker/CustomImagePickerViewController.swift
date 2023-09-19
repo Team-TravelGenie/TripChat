42023f7b513c4e42bb7297f889d691b6ae7c5a5d
summercat <summerly812@gmail.com>
2023-09-19 13:23:18 +0900

    style: 컨벤션 수정

--- TravelGenie/TravelGenie/Presentation/Chat/View/CustomImagePicker/CustomImagePickerViewController.swift
+++ TravelGenie/TravelGenie/Presentation/Chat/View/CustomImagePicker/CustomImagePickerViewController.swift
@@ -27,7 +27,6 @@ final class CustomImagePickerViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
     
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -35,8 +34,6 @@ final class CustomImagePickerViewController: UIViewController {
     deinit {
         PHPhotoLibrary.shared().unregisterChangeObserver(self)
     }
-    
-    // MARK: Override(s)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -71,7 +68,9 @@ final class CustomImagePickerViewController: UIViewController {
         collectionView.dataSource = self
         collectionView.backgroundColor = .clear
         collectionView.allowsMultipleSelection = true
-        collectionView.register(CustomImagePickerCell.self, forCellWithReuseIdentifier: CustomImagePickerCell.identifier)
+        collectionView.register(
+            CustomImagePickerCell.self,
+            forCellWithReuseIdentifier: CustomImagePickerCell.identifier)
     }
     
     private func configureHeaderView() {
