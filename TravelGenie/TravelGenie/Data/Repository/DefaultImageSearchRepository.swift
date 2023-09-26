1ebea70ded7b123629a1942422607ddeafcf057b
woong <workplayhard1@naver.com>
2023-09-26 23:14:03 +0900

    feat: DefaultImageSearchRepository가 ResponseError를 통해 에러처리하도록 수정구현

--- TravelGenie/TravelGenie/Data/Repository/DefaultImageSearchRepository.swift
+++ TravelGenie/TravelGenie/Data/Repository/DefaultImageSearchRepository.swift
@@ -14,16 +14,21 @@ final class DefaultImageSearchRepository: ImageSearchRepository {
     func searchImage(
         with tags: [Tag],
         spot: String,
-        completion: @escaping (Result<String, Error>) -> Void)
+        completion: @escaping (Result<String, ResponseError>) -> Void)
     {
         let query = tags.map { $0.value }.joined(separator: " ")
         let requestModel = ImageSearchRequestModel(q: query, exactTerms: spot)
         networkService.request(GoogleCustomSearchAPI.imageSearch(requestModel)) { result in
             switch result {
             case .success(let response):
-                completion(.success(response.items[0].link))
+                guard let link = response.items.first?.link else {
+                    completion(.failure(ResponseError.emptyResponse))
+                    return
+                }
+                
+                completion(.success(link))
             case .failure(let error):
-                completion(.failure(error))
+                completion(.failure(ResponseError.moyaError(error)))
             }
         }
     }
--- TravelGenie/TravelGenie/Domain/RepositoryInterface/ImageSearchRepository.swift
+++ TravelGenie/TravelGenie/Domain/RepositoryInterface/ImageSearchRepository.swift
@@ -9,5 +9,5 @@ protocol ImageSearchRepository {
     func searchImage(
         with tags: [Tag],
         spot: String,
-        completion: @escaping (Result<String, Error>) -> Void)
+        completion: @escaping (Result<String, ResponseError>) -> Void)
 }
