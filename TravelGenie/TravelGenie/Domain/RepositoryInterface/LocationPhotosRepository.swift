93aeb9b7daadfa5f10be661386a61320ccb368e5
woong <workplayhard1@naver.com>
2023-09-26 22:08:57 +0900

    feaet: Foundation import 제거

--- TravelGenie/TravelGenie/Domain/RepositoryInterface/LocationPhotosRepository.swift
+++ TravelGenie/TravelGenie/Domain/RepositoryInterface/LocationPhotosRepository.swift
@@ -5,8 +5,6 @@
 //  Created by 서현웅 on 2023/09/25.
 //
 
-import Foundation
-
 protocol LocationPhotosRepository {
     func searchLocation(
         query: String,
--- TravelGenie/TravelGenie/Domain/RepositoryInterface/TranslateRepository.swift
+++ TravelGenie/TravelGenie/Domain/RepositoryInterface/TranslateRepository.swift
@@ -5,8 +5,6 @@
 //  Created by 서현웅 on 2023/09/01.
 //
 
-import Foundation
-
 protocol TranslateRepository {
     func translate(
         with keywords: String,
