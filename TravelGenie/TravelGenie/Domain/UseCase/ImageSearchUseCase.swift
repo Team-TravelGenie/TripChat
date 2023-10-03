f384692489dea816c8e029d208a5d6c8c8c24e67
woong <workplayhard1@naver.com>
2023-10-03 21:12:43 +0900

    feat: ImageSearch 함수분리

--- TravelGenie/TravelGenie/Domain/UseCase/ImageSearchUseCase.swift
+++ TravelGenie/TravelGenie/Domain/UseCase/ImageSearchUseCase.swift
@@ -35,104 +35,65 @@ final class DefaultImageSearchUseCase: ImageSearchUseCase {
         item: OpenAIRecommendation.RecommendationItem,
         completion: @escaping (Result<Data, Error>) -> Void)
     {
-        let themeTags = tags.filter { $0.category == .theme }
-        let keywordTags = tags.filter { $0.category == .keyword }
-        let tags = themeTags + keywordTags
+        let tags = tags.filter { $0.category == .theme || $0.category == .keyword }
         
         if item.country == "한국" {
-            locationSearchRepository.searchLocation(query: item.spotKorean, languageCode: .korean) { [weak self] result in
-                switch result {
-                case .success(let locationID):
-                    self?.locationPhotosRepository.searchPhoto(locationID: locationID, languageCode: .korean) { result in
-                        switch result {
-                        case .success(let imageURL):
-                            ImageManager.retrieveImage(with: imageURL) { data in
-                                completion(.success(data))
-                            }
-                        case .failure(let error):
-                            completion(.failure(error))
-                        }
-                    }
-                case .failure(let locationSearchError):
-                    self?.imageSearchRepository.searchImage(with: tags, spot: item.spotKorean, completion: { result in
-                        switch result {
-                        case .success(let imageURL):
-                            ImageManager.retrieveImage(with: imageURL) { data in
-                                completion(.success(data))
-                            }
-                        case .failure(let error):
-                            completion(.failure(error))
-                        }
-                    })
-                    
-                    completion(.failure(locationSearchError))
-                }
-            }
+            handleLocationSearch(query: item.spotKorean, languageCode: .korean, tags: tags, spot: item.spotKorean, completion: completion)
         } else {
-            locationSearchRepository.searchLocation(query: item.spotEnglish, languageCode: .english) { [weak self] result in
-                switch result {
-                case .success(let locationID):
-                    self?.locationPhotosRepository.searchPhoto(locationID: locationID, languageCode: .english) { result in
-                        switch result {
-                        case .success(let imageURL):
-                            ImageManager.retrieveImage(with: imageURL) { data in
-                                completion(.success(data))
-                            }
-                        case .failure(let error):
-                            completion(.failure(error))
-                        }
-                    }
-                case .failure(let locationSearchError):
-                    self?.imageSearchRepository.searchImage(with: tags, spot: item.spotEnglish, completion: { result in
-                        switch result {
-                        case .success(let imageURL):
-                            ImageManager.retrieveImage(with: imageURL) { data in
-                                completion(.success(data))
-                            }
-                        case .failure(let error):
-                            completion(.failure(error))
-                        }
-                    })
-                    
-                    completion(.failure(locationSearchError))
-                }
+            handleLocationSearch(query: item.spotEnglish, languageCode: .english, tags: tags, spot: item.spotEnglish, completion: completion)
+        }
+    }
+    
+    private func handleLocationSearch(
+        query: String,
+        languageCode: LanguageCode,
+        tags: [Tag],
+        spot: String,
+        completion: @escaping (Result<Data, Error>) -> Void)
+    {
+        locationSearchRepository.searchLocation(query: query, languageCode: languageCode) { [weak self] result in
+            switch result {
+            case .success(let locationID):
+                self?.handleLocationPhotoSearch(locationID: locationID, languageCode: languageCode, tags: tags, spot: spot, completion: completion)
+            case .failure:
+                self?.handleImageSearch(tags: tags, spot: spot, completion: completion)
             }
         }
     }
+    
+    private func handleLocationPhotoSearch(
+        locationID: String,
+        languageCode: LanguageCode,
+        tags: [Tag],
+        spot: String,
+        completion: @escaping (Result<Data, Error>) -> Void)
+    {
+        locationPhotosRepository.searchPhoto(locationID: locationID, languageCode: languageCode) { [weak self] result in
+            switch result {
+            case .success(let imageURL):
+                ImageManager.retrieveImage(with: imageURL) { data in
+                    completion(.success(data))
+                }
+            case .failure:
+                self?.handleImageSearch(tags: tags, spot: spot, completion: completion)
+            }
+        }
+    }
+    
+    private func handleImageSearch(
+        tags: [Tag],
+        spot: String,
+        completion: @escaping (Result<Data, Error>) -> Void)
+    {
+        imageSearchRepository.searchImage(with: tags, spot: spot, completion: { result in
+            switch result {
+            case .success(let imageURL):
+                ImageManager.retrieveImage(with: imageURL) { data in
+                    completion(.success(data))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        })
+    }
 }
-/*
- 
- 1. 만약, item.country == "한국"이면, item.spotkorean 으로 tripadvisor에 요청을보낸다.
- 2. 결과값이 empty이면, googleSearch에 태그 + spotkorean을 검색어로 보낸다.
- 3. else(해외 일 경우),
- 4. tripadvisor에 spotEnglish로 locationSearch를 보낸다.
- 5. 결과값이 empty이면, googleSearch에 태그 + spotEnglish를 검색어로 보낸다.
- 
- */
-
-/*
- locationSearchRepository.searchLocation(query: item.spotKorean, languageCode: "kr") { result in
- switch result {
- case .success(let imageURL):
- ImageManager.retrieveImage(with: imageURL) { data in
- completion(.success(data))
- }
- case .failure(let error):
- if case ResponseError.emptyResponse = error {
- self.imageSearchRepository.searchImage(with: tags, spot: item.spotKorean) { result in
- switch result {
- case .success(let imageURL):
- ImageManager.retrieveImage(with: imageURL) { data in
- completion(.success(data))
- }
- case .failure(let error):
- completion(.failure(error))
- }
- }
- }
- }
- }
- } else {
- locationSearchRepository.sear
- }
- */
